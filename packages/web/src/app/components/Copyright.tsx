652cafecc107d7d5e503910f468782acee6b5d56
Jun Zheng <me@junzheng.dev>
2022-02-23 20:51:04 -0500

    Change script to unknown-local-build

--- packages/web/src/app/components/Copyright.tsx
+++ packages/web/src/app/components/Copyright.tsx
@@ -3,7 +3,7 @@ export function Copyright() {
     <div className="text-xs text-gray-500 text-center">
       &copy; Tinybox 2022
       <br />
-      Build: {process.env['NX_BUILD'] || 'unknown-build'}
+      Build: {process.env['NX_BUILD'] || 'unknown-local-build'}
     </div>
   );
 }
